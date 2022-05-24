560ec3230dae8b94387c605091d188ded7fb75df
KubraNurGursoyyy <kubranurgursoyy@hotmail.com>
2022-05-24 17:58:48 +0300

    added cross origin annotation

--- src/main/java/com/testcase/Report/Controller/ReportController.java
+++ src/main/java/com/testcase/Report/Controller/ReportController.java
@@ -4,12 +4,14 @@ import com.testcase.Report.Entity.Report;
 import com.testcase.Report.Service.ReportService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/api/report")
 @RequiredArgsConstructor
--- src/main/java/com/testcase/TestcaseApplication.java
+++ src/main/java/com/testcase/TestcaseApplication.java
@@ -2,6 +2,9 @@ package com.testcase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
 
 @SpringBootApplication
 public class TestcaseApplication {
