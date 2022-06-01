717f0194ee22e019e2684b5651d72e4bfcb87257
KubraNurGursoyyy <kubranurgursoyy@hotmail.com>
2022-06-01 13:25:25 +0300

    name filter

--- src/main/java/com/testcase/Products/Repository/ProductsRepository.java
+++ src/main/java/com/testcase/Products/Repository/ProductsRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 @Repository
 public interface ProductsRepository extends JpaRepository<Products,Integer> {
     List<Products> findAllByWarehouseId(Integer id);
-    List<Products> findAllByProductsContains(String products);
 
 }
